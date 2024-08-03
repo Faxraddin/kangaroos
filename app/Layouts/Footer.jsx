6ce333c59e3acc78b1e8d582cbf34de90f771dca
Faxraddin <reydivonay@gmail.com>
2024-08-03 19:39:02 +0400

    social btns

--- app/Layouts/Footer.jsx
+++ app/Layouts/Footer.jsx
@@ -21,11 +21,20 @@ const Footer = () => {
         <button className="btn-animation sm:px-0 sm:w-full lg:text-[18px] lg:mt-0 mt-5 py-3 px-20 text-[1.3vw] text-white rounded-r-[20px] rounded-t-[20px]">Get Started</button>
       </div>
       <div className="w-full sm:flex-col sm:gap-5 sm:items-start h-auto flex justify-between items-center">
-        <span className="text-gray-400 lg:text-[16px] text-[1.3vw]">@Copyright 2024. All Right Reserved</span>
-        <div className="text-[2.3vw] flex justify-center gap-11 items-center">
-          <span className="text-white lg:text-[26px] rounded-full yellow p-3"><LuInstagram/></span>
-          <span className="text-white lg:text-[26px] rounded-full yellow p-3"><FaFacebook /></span>
-          <span className="text-white lg:text-[26px] rounded-full yellow p-3"><FaLinkedin /></span>
+        <span className="text-gray-400 lg:text-[16px] text-[1.1vw]">@Copyright 2024. All Right Reserved</span>
+        <div className="text-[2vw] xs:w-full xs:justify-between flex justify-center gap-11 items-center">
+          <span className="text-white cursor-pointer group border-2 relative border-[#FDC110] lg:text-[26px] rounded-full w-[3.5vw] lg:w-[50px] lg:h-[50px] h-[3.5vw] flex justify-center items-center">
+            <LuInstagram className="z-10 group-hover:text-black ease-linear transition-all duration-200" />
+            <div className="w-full h-full ease-linear transition-all duration-200 group-hover:-translate-y-[90%] yellow rounded-full absolute left-0 top-0 z-0"></div>
+          </span>
+          <span className="text-white cursor-pointer group border-2 relative border-[#FDC110] lg:text-[26px] rounded-full w-[3.5vw] lg:w-[50px] lg:h-[50px] h-[3.5vw] flex justify-center items-center">
+            <FaFacebook className="z-10 group-hover:text-black ease-linear transition-all duration-200" />
+            <div className="w-full h-full ease-linear transition-all duration-200 group-hover:-translate-y-[90%] yellow rounded-full absolute left-0 top-0 z-0"></div>
+          </span>
+          <span className="text-white cursor-pointer group border-2 relative border-[#FDC110] lg:text-[26px] rounded-full w-[3.5vw] lg:w-[50px] lg:h-[50px] h-[3.5vw] flex justify-center items-center">
+            <FaLinkedin className="z-10 group-hover:text-black ease-linear transition-all duration-200" />
+            <div className="w-full h-full ease-linear transition-all duration-200 group-hover:-translate-y-[90%] yellow rounded-full absolute left-0 top-0 z-0"></div>
+          </span>
         </div>
       </div>
     </footer>
