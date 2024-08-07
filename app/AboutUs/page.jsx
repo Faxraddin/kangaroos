a5d85a5031e360578267db9039a237cf1c344366
Faxraddin <reydivonay@gmail.com>
2024-08-07 15:28:24 +0400

    sd

--- app/AboutUs/page.jsx
+++ app/AboutUs/page.jsx
@@ -35,9 +35,9 @@ const AboutUs = () => {
         <div className="md:px-5 Font2 gap-20 lg:gap-5 lg:px-10 w-full h-full flex justify-center items-center flex-col px-20">
             <section className="w-full sm:mt-5 h-auto gap-10 flex justify-center flex-col items-center">
                 <div className="md:flex-col md:gap-5 md:justify-center w-full gap-10 h-auto flex justify-between items-center">
-                    <p className="md:w-full sm:text-[20px] md:text-[30px] Font2 w-[30%] uppercase text-[1.6vw] md:text-center  text-left">Children must <br className="md:hidden" /> learn from <br className="md:hidden" /> native speaksers!</p>
+                    <p className="md:w-full sm:text-[20px] md:text-[30px] w-[30%] uppercase text-[1.6vw] md:text-center  text-left">Children must <br className="md:hidden" /> learn from <br className="md:hidden" /> native speaksers!</p>
                     <Image className="w-[17vw] sm:w-[200px] md:w-[300px] h-auto" alt="hero h1 about us" width={500} height={500} src={'/About/h1.svg'} />
-                    <p className="md:text-[30px] sm:text-[19px] md:w-full Font1 w-[30%] text-[1.5vw] md:text-center text-right">The first lesson for children serves as a trial session, and it's completely complimentary!</p>
+                    <p className="md:text-[30px] sm:text-[19px] md:w-full w-[30%] text-[1.5vw] md:text-center text-right">The first lesson for children serves as a trial session, and it's completely complimentary!</p>
                 </div>
                 <div className="flex flex-col w-full justify-center items-center h-auto">
                     <h1 className="uppercase md:text-[13vw] text-[12vw]">Kangaroos</h1>
@@ -108,8 +108,8 @@ const AboutUs = () => {
 
             <section className="w-full md:flex-col md:justify-center h-auto flex justify-between items-center">
                 <div className="w-[45%] md:w-full h-[38vw] lg:h-auto flex-col lg:gap-3 pb-3 flex justify-between items-start">
-                    <h1 className="Font2 md:text-[30px] text-[5vw]">Sign Up for a free lesson now!</h1>
-                    <p className="text-[2vw] md:text-[13px]">start now and see what suits your child the best!</p>
+                    <h1 className="md:text-[30px] text-[4.5vw] Font1">Sign Up for a free lesson now!</h1>
+                    <p className="text-[2vw] md:text-[13px] text-gray-400">start now and see what suits your child the best!</p>
                     <button className="btn-animation md:text-[13px] sm:px-0 sm:w-full  sm:text-[13px] lg:py-2 py-3 px-20 text-[1.3vw] text-white rounded-r-[20px] rounded-t-[20px]">Get Started</button>
                 </div>
                 <div className="w-[50%] md:w-full lg:h-auto h-[38vw] rounded-[15px]">
@@ -141,7 +141,7 @@ const AboutUs = () => {
             </section>
 
             <section className="flex-col gap-10 sm:gap-5 w-full h-auto flex justify-center items-center">
-                <h1 className="font-bold Font1 sm:text-[25px] uppercase lg:w-full w-[50%] lg:text-[45px] text-[3.5vw]">Why Choose Us ?</h1>
+                <h1 className="font-bold Font1 sm:text-[25px] uppercase lg:w-full w-[100%] text-center lg:text-[45px] text-[3.5vw]">Why Choose Us ?</h1>
                 <div className="flex justify-between lg:gap-5 lg:flex-col lg:justify-center items-center w-full h-auto">
                     <div className="w-[30%] sm:p-5 lg:w-[100%] text-white bg-[#f55712] flex rounded-[10px] flex-col justify-center items-center gap-5 h-auto p-10">
                         <PiShootingStarFill className="text-[5vw] lg:text-[42px] sm:text-[36px]" />
