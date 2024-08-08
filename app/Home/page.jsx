a418b96110fb771e309b4852e7071a5395a9c35d
Faxraddin <reydivonay@gmail.com>
2024-08-08 13:56:57 +0400

    fa

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
 import React from "react";
-import { RxArrowTopRight,RxStarFilled } from "react-icons/rx";
+import { RxArrowTopRight, RxStarFilled } from "react-icons/rx";
 import { FaStar } from "react-icons/fa6";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { FaHeart } from "react-icons/fa";
 
 const HomePage = () => {
+    const reviews = [
+        { id: 0, name: 'Leo', text: 'Dear Hannah, Love you so much! You always bring lots of fun to Leo , even sometimes he felt a bit down to take lesson after whole day in school! Leo is becoming very comfortable with the process of the classes and he is showing a lot of improvement!' ,img:''},
+        { id: 0, name: 'Chelsea', text: "Teacher Hannah is kind, friendly and patient with my twin girls. She is very good at assessing a student's abilities and interests and adjusting her lessons accordingly. She makes the lessons fun and challenging. I would highly recommend her as a teacher.",img:'' },
+        { id: 0, name: 'Bella', text: 'Hi Hannah. Thank you very much for taking care of Bella in drama class. She enjoyed all activities, developed her thinking, got a lot of fun in your class. She adapted quickly and smoothly under your training. Thank you again for helping her improve all abilities. Hugs and kisses!',img:'' }
+    ]
     return (
         <div className="md:px-5 Font2 gap-20 lg:gap-5 lg:px-10 w-full h-full flex justify-center items-center flex-col px-20">
             <section className="lg:flex-col w-full h-auto flex justify-between items-center">
                 <div className="lg:w-full lg:gap-5 flex gap-[2.5vw] flex-col h-auto w-1/2">
                     <div className="h-auto lg:w-full sm:text-left sm:items-start sm:justify-start lg:justify-center lg:items-center lg:text-center lg:gap-5 w-full flex-col flex">
-                        <h1 className="font-bold sm:text-[35px] lg:text-[45px] text-[4.4vw]">Where <span className="relative">English <Image alt="underline" width={400} height={400} className="w-[30vw] sm:w-[230px] absolute sm:-bottom-3 -bottom-6 right-0 h-auto" src={'/Underline.png'} /></span> <br className="hidden sm:flex"/> Meets Creativity </h1>
+                        <h1 className="font-bold sm:text-[35px] lg:text-[45px] text-[4.4vw]">Where <span className="relative">English <Image alt="underline" width={400} height={400} className="w-[30vw] sm:w-[230px] absolute sm:-bottom-3 -bottom-6 right-0 h-auto" src={'/Underline.png'} /></span> <br className="hidden sm:flex" /> Meets Creativity </h1>
                         <p className="text-[1.3vw] lg:w-full lg:text-[18px] sm:text-[13px] text-gray-400 w-[80%]">- Traditional methods meet new and diverse creativity to bring an empowering online English experience to your children and family.</p>
                         <button className="sm:w-full lg:text-[18px] sm:text-[13px] sm:py-2 lg:mt-0 mt-5 py-3 w-[50%] text-[1.3vw] text-white rounded-l-[20px] rounded-t-[20px] btn-animation">15 MIN FREE, NOW?</button>
                     </div>
@@ -42,7 +47,7 @@ const HomePage = () => {
                     <div className="w-[31%] 2lg:w-[48%] h-auto relative">
                         <Image className="w-full h-auto" src={'/Home/h3.svg'} width={2000} height={2000} alt="h3" />
                         <div className="absolute sm:top-[3vw] sm:left-[3vw] w-full flex flex-col gap-2 h-auto left-8 top-8">
-                            <h1 className="text-black 2lg:text-[4vw] sm:text-[3.7vw] text-[2.4vw] font-semibold">Early Years <br/> <span className="flex items-center gap-2"><FaArrowRightLong/> Teens</span></h1>
+                            <h1 className="text-black 2lg:text-[4vw] sm:text-[3.7vw] text-[2.4vw] font-semibold">Early Years <br /> <span className="flex items-center gap-2"><FaArrowRightLong /> Teens</span></h1>
                             <p className="text-[1.1vw] sm:hidden 2lg:text-[1.5vw] w-[80%] text-gray-400">Pre A1- C2 - supporting students through an extensive language learning journey</p>
                         </div>
                         <RxArrowTopRight className="text-[4vw] 2lg:text-[7vw] 2lg:bottom-[2.5vw] 2lg:right-[2.5vw] md:right-[3vw] md:bottom-[3.5vw] yellow-text absolute bottom-[2vw] right-[2vw]" />
@@ -69,12 +74,12 @@ const HomePage = () => {
                 <div className="lg:flex-col sm:gap-3 sm:text-left lg:text-center w-full relative flex items-start justify-between gap-10">
                     <div className="font-bold Font1 sm:flex-wrap sm:text-[25px] lg:flex lg:justify-center sm:justify-start lg:w-full w-[50%] lg:text-[45px] text-[3.5vw]">
                         <div className="flex sm:flex-wrap items-center gap-3">
-                            Why <span className="relative">Choose <Image alt="underline" width={400} height={400} className="w-[45vw] sm:w-[230px] lg:w-[300px] sm:-bottom-1 lg:-bottom-4 absolute -bottom-3 right-0 h-auto" src={'/Underline.png'} /></span> 
-                            <div className="flex sm:flex-wrap items-center gap-3"><span className="flex items-center gap-2">Us ? <RxStarFilled className="yellow-text text-[3vw"/> </span> </div>
+                            Why <span className="relative">Choose <Image alt="underline" width={400} height={400} className="w-[45vw] sm:w-[230px] lg:w-[300px] sm:-bottom-1 lg:-bottom-4 absolute -bottom-3 right-0 h-auto" src={'/Underline.png'} /></span>
+                            <div className="flex sm:flex-wrap items-center gap-3"><span className="flex items-center gap-2">Us ? <RxStarFilled className="yellow-text text-[3vw" /> </span> </div>
                         </div>
                     </div>
-                    <p className="text-[1.3vw] w-[45%] lg:w-full lg:text-[18px] sm:text-[13px] text-gray-400">We are actively, passionately, creatively paving the way for multi-lingual speakers children worldwide 
- through native education standards. Shaping the world of online education through experience, understanding and relatability.</p>
+                    <p className="text-[1.3vw] w-[45%] lg:w-full lg:text-[18px] sm:text-[13px] text-gray-400">We are actively, passionately, creatively paving the way for multi-lingual speakers children worldwide
+                        through native education standards. Shaping the world of online education through experience, understanding and relatability.</p>
                     <Image src={'/Curve2.svg'} alt="curve3" className="w-[12vw] lg:-rotate-45 lg:w-[90px] sm:hidden lg:relative lg:top-0 lg:left-0 h-auto absolute right-0 top-[11vw]" width={400} height={400} />
                 </div>
                 <div className="w-full flex h-auto justify-start items-center">
@@ -83,56 +88,32 @@ const HomePage = () => {
             </section>
             <section className="flex-col sm:gap-5 lg:gap-10 gap-28 big:gap-40 w-full h-auto flex justify-center items-center">
                 <div className="lg:flex-col sm:gap-3 sm:text-left lg:text-center w-full relative flex items-center justify-between gap-10">
-                    <h1 className="font-bold sm:text-[25px] lg:w-full w-[50%] Font1 lg:text-[45px] text-[3.5vw]">What Our <span className="relative">Lovely <Image alt="underline" width={400} height={400} className="w-[45vw] sm:w-[230px] lg:w-[300px] lg:-bottom-4 absolute -bottom-4 right-0 h-auto" src={'/Underline.png'} /></span> <br className="hidden sm:flex"/> <span className="flex items-center gap-2"> Clients Say <FaHeart className="orange"/></span></h1>
+                    <h1 className="font-bold sm:text-[25px] lg:w-full w-[50%] Font1 lg:text-[45px] text-[3.5vw]">What Our <span className="relative">Lovely <Image alt="underline" width={400} height={400} className="w-[45vw] sm:w-[230px] lg:w-[300px] lg:-bottom-4 absolute -bottom-4 right-0 h-auto" src={'/Underline.png'} /></span> <br className="hidden sm:flex" /> <span className="flex items-center gap-2"> Clients Say <FaHeart className="orange" /></span></h1>
                     <p className="text-[1.3vw] w-[45%] lg:w-full lg:text-[18px] sm:text-[13px] text-gray-400">We recently heard from Haining Wei, who started teaching with Swing in the spring of 2022. Below, she shares her story of how she was able to get into the classroom quickly.</p>
                     <Image src={'/Lines.png'} alt="lines" className="w-[15vw] -rotate-12 lg:w-[110px] lg:hidden h-auto absolute right-0 top-[12vw]" width={400} height={400} />
                 </div>
                 <div className="w-full sm:flex-col 2lg:justify-center 2lg:gap-5 2lg:flex-wrap flex h-auto justify-between items-center">
-                    <div className="w-[31%] sm:w-full 2lg:w-[48%] h-auto relative">
-                        <Image className="w-full h-auto" src={'/Home/Review.svg'} width={2000} height={2000} alt="h3" />
-                        <div className="absolute w-auto sm:right-[5vw] sm:top-[5vw] flex flex-col gap-1 items-end h-auto right-[2vw] top-[1vw]">
-                            <h2 className="font-semibold sm:text-[5vw] 2lg:text-[2vw] text-[1.7vw]">James Hoven</h2>
-                            <div className="text-[2vw] sm:gap-[1vw] sm:right-[5vw] sm:text-[7vw] 2lg:text-[3vw] yellow-text flex justify-center items-start gap-3">
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
+                    {reviews.map((review,index) => {
+                        return (
+                            <div key={index} className="w-[31%] sm:w-full 2lg:w-[48%] h-auto relative">
+                                <Image className="w-full h-auto" src={'/Home/Review.svg'} width={2000} height={2000} alt="h3" />
+                                <div className="absolute w-auto sm:right-[5vw] sm:top-[5vw] flex flex-col gap-1 items-end h-auto right-[2vw] top-[1vw]">
+                                    <h2 className="font-semibold sm:text-[5vw] 2lg:text-[2vw] text-[1.7vw]">{review.name}</h2>
+                                    <div className="text-[2vw] sm:gap-[1vw] sm:right-[5vw] sm:text-[7vw] 2lg:text-[3vw] yellow-text flex justify-center items-start gap-3">
+                                        <FaStar />
+                                        <FaStar />
+                                        <FaStar />
+                                        <FaStar />
+                                        <FaStar />
+                                    </div>
+                                </div>
+                                <p className="w-[80%] sm:text-[4vw] sm:left-[5vw] sm:bottom-[5vw] 2lg:w-[90%] 2lg:text-[2vw] text-[1.1vw] font-light absolute bottom-[3vw] left-[2vw]">{review.text}</p>
                             </div>
-                        </div>
-                        <p className="w-[80%] sm:text-[4vw] sm:left-[5vw] sm:bottom-[5vw] 2lg:w-[90%] 2lg:text-[2vw] text-[1.1vw] font-light absolute bottom-[3vw] left-[2vw]">Hi Hannah. Thank you very much for taking care of Bella in drama class. She enjoyed all activities, developed her thinking, got a lot of fun in your class. She adapted quickly and smoothly under your training. Thank you again for helping her improve all abilities. Hugs and kisses!</p>
-                    </div>
-                    <div className="w-[31%] sm:w-full 2lg:w-[48%] h-auto relative">
-                        <Image className="w-full h-auto" src={'/Home/Review.svg'} width={2000} height={2000} alt="h3" />
-                        <div className="absolute w-auto sm:right-[5vw] sm:top-[5vw] flex flex-col gap-1 items-end h-auto right-[2vw] top-[1vw]">
-                            <h2 className="font-semibold sm:text-[5vw] 2lg:text-[2vw] text-[1.7vw]">James Hoven</h2>
-                            <div className="text-[2vw] sm:gap-[1vw] sm:right-[5vw] sm:text-[7vw] 2lg:text-[3vw] yellow-text flex justify-center items-start gap-3">
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                            </div>
-                        </div>
-                        <p className="w-[80%] sm:text-[4vw] sm:left-[5vw] sm:bottom-[5vw] 2lg:w-[90%] 2lg:text-[2vw] text-[1.1vw] font-light absolute bottom-[3vw] left-[2vw]">Teacher Hannah is kind, friendly and patient with my twin girls. She is very good at assessing a student's abilities and interests and adjusting her lessons accordingly. She makes the lessons fun and challenging. I would highly recommend her as a teacher.</p>
-                    </div>
-                    <div className="w-[31%] sm:w-full 2lg:w-[48%] h-auto relative">
-                        <Image className="w-full h-auto" src={'/Home/Review.svg'} width={2000} height={2000} alt="h3" />
-                        <div className="absolute w-auto sm:right-[5vw] sm:top-[5vw] flex flex-col gap-1 items-end h-auto right-[2vw] top-[1vw]">
-                            <h2 className="font-semibold sm:text-[5vw] 2lg:text-[2vw] text-[1.7vw]">James Hoven</h2>
-                            <div className="text-[2vw] sm:gap-[1vw] sm:right-[5vw] sm:text-[7vw] 2lg:text-[3vw] yellow-text flex justify-center items-start gap-3">
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                                <FaStar/>
-                            </div>
-                        </div>
-                        <p className="w-[80%] sm:text-[4vw] sm:left-[5vw] sm:bottom-[5vw] 2lg:w-[90%] 2lg:text-[2vw] text-[1.1vw] font-light absolute bottom-[3vw] left-[2vw]">Dear Hannah, Love you so much! You always bring lots of fun to Leo , even sometimes he felt a bit down to take lesson after whole day in school! Leo is becoming very comfortable with the process of the classes and he is showing a lot of improvement!</p>
-                    </div>
+                        )
+                    })}
                 </div>
             </section>
-            
+
         </div>
     )
 }
