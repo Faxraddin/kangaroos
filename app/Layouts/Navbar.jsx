71d34c736f40eefc04bc9af2422b6ca41b3bc1ad
Faxraddin <reydivonay@gmail.com>
2024-09-28 09:47:22 +0200

    n

--- app/Layouts/Navbar.jsx
+++ app/Layouts/Navbar.jsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import Button from "@/components/Button";
-import { usePathname,useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
   const navigate = useRouter();
   const pathname = usePathname();
-  const [navActive,setNavActive] = useState(false)
+  const [navActive, setNavActive] = useState(false);
   const [color, setColor] = useState('#f55712');
 
   useEffect(() => {
@@ -21,26 +21,30 @@ const Navbar = () => {
         setColor('#6A2985');
         break;
       default:
-        setColor('#f55712'); 
+        setColor('#f55712');
     }
   }, [pathname]);
 
   const handleClick = () => {
-    setNavActive(!navActive)
-  }
+    setNavActive(!navActive);
+  };
+
+  const handleLinkClick = () => {
+    setTimeout(()=>{setNavActive(false)},500)
+  };
 
   return (
     <header className="w-full h-auto p-2 flex justify-center items-center">
       <div className="md:px-5 lg:px-10 flex justify-between items-center h-auto w-full px-20">
-        <Image onClick={()=>{navigate('/')}} width={500} height={500} alt="logo" src={'/Logo.svg'} className="w-[6vw] border-[#fdc110] border-2 rounded-full sm:w-[70px] sm:-ml-2 lg:w-[90px] h-auto" />
+        <Image onClick={() => { navigate('/'); }} width={500} height={500} alt="logo" src={'/Logo.svg'} className="w-[6vw] border-[#fdc110] border-2 rounded-full sm:w-[70px] sm:-ml-2 lg:w-[90px] h-auto" />
         <ul className="text-[1.1vw] lg:hidden Font2 w-auto font-light flex justify-center gap-10 items-center">
-          <Link className={`${pathname === '/' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Home</Link>
-          <Link className={`${pathname === '/AboutUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/AboutUs'}>About Us</Link>
-          <Link className={`${pathname === '/Education' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Education</Link>
-          <Link className={`${pathname === '/Price' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/Price'}>Pricing</Link>
-          <Link className={`${pathname === '/ContactUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/ContactUs'}>Contact Us</Link>
+          <Link onClick={handleLinkClick} className={`${pathname === '/' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Home</Link>
+          <Link onClick={handleLinkClick} className={`${pathname === '/AboutUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/AboutUs'}>About Us</Link>
+          <Link onClick={handleLinkClick} className={`${pathname === '/Education' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Education</Link>
+          <Link onClick={handleLinkClick} className={`${pathname === '/Price' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/Price'}>Pricing</Link>
+          <Link onClick={handleLinkClick} className={`${pathname === '/ContactUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/ContactUs'}>Contact Us</Link>
         </ul>
-        <Button className="py-3 px-7 lg:hidden  text-[1vw]" text={'Start Now!'} />
+        <Button className="py-3 px-7 lg:hidden text-[1vw]" text={'Start Now!'} />
         <div className="w-auto h-auto z-30 hidden rounded-full p-3 sm:bg-transparent yellow lg:flex">
           <label className="hamburger">
             <input onClick={handleClick} type="checkbox" />
@@ -53,15 +57,16 @@ const Navbar = () => {
 
         <div className={`${navActive ? 'translate-x-[-5%] overflow-hidden sm:translate-x-[-8%]' : 'translate-x-[100%]'} overflow-hidden w-[110vw] ease-linear duration-300 transition-all h-screen bg-[black] top-0 z-20 fixed`}>
           <ul className="text-[20px] h-full text-[#fdc110] flex-col overflow-hidden Font2 w-full font-light flex justify-center gap-10 items-center">
-            <Link className={`${pathname === '/' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Home</Link>
-            <Link className={`${pathname === '/AboutUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/AboutUs'}>About Us</Link>
-            <Link className={`${pathname === '/Education' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Education</Link>
-            <Link className={`${pathname === '/Price' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/Price'}>Pricing</Link>
-            <Link className={`${pathname === '/ContactUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/ContactUs'}>Contact Us</Link>
+            <Link onClick={handleLinkClick} className={`${pathname === '/' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Home</Link>
+            <Link onClick={handleLinkClick} className={`${pathname === '/AboutUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/AboutUs'}>About Us</Link>
+            <Link onClick={handleLinkClick} className={`${pathname === '/Education' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/'}>Education</Link>
+            <Link onClick={handleLinkClick} className={`${pathname === '/Price' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/Price'}>Pricing</Link>
+            <Link onClick={handleLinkClick} className={`${pathname === '/ContactUs' ? 'font-bold scale-125' : 'text-gray-400 scale-100'}`} href={'/ContactUs'}>Contact Us</Link>
           </ul>
         </div>
       </div>
     </header>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
